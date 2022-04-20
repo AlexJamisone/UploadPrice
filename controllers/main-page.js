31cee4a8ab5a7e5673373086fe4d41af950841e9
AlexJamisone <business.homeit@gmail.com>
2022-04-20 22:30:31 +0300

    refrech Date in ipad

--- controllers/main-page.js
+++ controllers/main-page.js
@@ -1,7 +1,7 @@
 export function getMainPage(req, res, next) {
     res.render('home', {
         pageTitle: 'Home Page',
-        time: new Date().toLocaleTimeString().slice(0, -3),
+        time: new Date().toLocaleTimeString( {timeZone: "Europe/Moscow"}).slice(0, -3),
         weekDay: new Intl.DateTimeFormat('ru-RU', { weekday: 'short'}).format(),
         weekNum: new Intl.DateTimeFormat('ru-RU', { day: 'numeric'}).format(),
         month: new Intl.DateTimeFormat('ru-RU', { month: 'short'}).format(),
